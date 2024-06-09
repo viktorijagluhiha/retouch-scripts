e3ec4850e49f05394622d58c2549e4c3ab644da5
Viktorija Gluhiha <viktorija.gluhiha@gmail.com>
2024-06-09 03:45:55 +0300

    Fixed bug in logging file path (hardcoded prefix)

--- process/copy_nefs.bat
+++ process/copy_nefs.bat
@@ -4,7 +4,7 @@ set prefix=DSC_
 set extension=.NEF
 echo V. Gluhih production
 for /F "usebackq eol=| delims=" %%A in ("%file%") do (
-  echo %~dp0DSC_%%~nxA\%prefix%%%~nxA%extension%
+  echo %~dp0%prefix%%%~nxA\%prefix%%%~nxA%extension%
   if not exist "%~dp0%prefix%%%~nxA" (
     mkdir "%~dp0%prefix%%%~nxA"
     copy "%~dp0..\%prefix%%%~nxA%extension%" "%~dp0%prefix%%%~nxA\%prefix%%%~nxA%extension%"
